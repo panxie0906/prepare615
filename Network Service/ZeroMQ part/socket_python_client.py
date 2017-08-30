ed2349fbcf5cfabb869fb54f3277de97ab86a2e5
xiepan <406551408@qq.com>
2017-08-30 08:37:09 +0800

    better the socket code

--- Network Service/ZeroMQ part/socket_python_client.py
+++ Network Service/ZeroMQ part/socket_python_client.py
@@ -19,6 +19,14 @@ socket.sendall(bytes[,flags]) unlike send(), it continues to send data from byte
 socket.sendto(bytes,flags,address)
 '''
 
+#测试了一个客户端的情况：服务器多次往客户端发送数据，实际上是发送到缓冲区中去，客户端每次从缓冲区recv bufsize长度的数据
+'''
+还有一下情况没有测试：
+1.如果服务器向多个客户端发送data，所用的缓冲区是一个吗？
+2.如果多个客户端向服务器发送data，所用的缓冲区是同一个吗？
+3.如果发送速度很快，导致缓冲区长度不够了怎么办？在看文档的过程中看到了阻塞和非阻塞形式的发送，可以考虑试一试
+'''
+
 def socket_connect():
     addr = ('localhost',6379)
     c = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -28,22 +36,33 @@ def socket_connect():
 def data_recv(length,c):
     data = ""
     while length > 0:
-        if length > 1024:
-            data = data + c.recv(1024).decode()
-            length = length - 1024
+        if length > 3072:
+            #不知道是什么错误，有可能是recv（）到了数据末端？对socket的机制还是有点不那么熟悉
+            #看到一句话
+            #出现错误的原因是因为utf8格式一般是'\xef', '\xbb', '\xbf'三个一起出现，然后解析的，而在某处不能够成三个一组的时候就会出现这样的错误
+            #果然如此，将1024换成3072即可
+            data_tmp = c.recv(3072).decode()
+            data = data + data_tmp
+            length = length - 3072
         else:
-            #之前忘了将recv（）参数改为length，导致错误
-            data = data + c.recv(length).decode()
-            length = length -1024
+            #之前忘了将recv（）参数改为length，导致错误 unexpected end of data
+            data_tmp = c.recv(length).decode()
+            data = data + data_tmp
+            length = length -3072
         
     return data
 
 if __name__ == '__main__':
     c = socket_connect()
     
-    data_bytes = c.recv(1024)
-    data_str = data_bytes.decode()
-    print(data_str,int(data_str[4:12]))
-    data_recved = data_recv(int(data_str[4:12]),c)
-    print(data_recved)
-    
+    while True:
+        try:
+            data_bytes = c.recv(3072)
+            data_str = data_bytes.decode()
+            if data_str[0:4].isdigit():
+                data_recved = data_recv(int(data_str[4:12]),c)
+                print(data_recved)
+            else:
+                print('error')
+        except EOFError:
+            print('EOFError')
--- Network Service/ZeroMQ part/socket_python_server.py
+++ Network Service/ZeroMQ part/socket_python_server.py
@@ -3,6 +3,7 @@
 import socket
 import json
 import os
+#import chardet #通过pip install chardet 获取,其作用是获取bytes或者bytesarray的编码类型
 
 def socket_bind():
     addr = ('localhost',6379)
@@ -20,26 +21,34 @@ def get_bytes():
     #print(os.path.dirname(__file__))
     base_path = os.path.dirname(__file__)
     file_path = os.path.join(base_path,"configuration.xml")  
-    print(file_path)
+    file_path1 = os.path.join(base_path,"baspools.xml") 
+    
     # 此处有必要加上encoding='utf-8'否则容易报错
     file_object = open(file_path,'r',encoding='utf-8')
+    file_object1 = open(file_path1,'r',encoding='utf-8')
     xml_str = file_object.read()
+    xml_str1 = file_object1.read()
     
     xml_bytes = xml_str.encode(encoding='utf-8')
-    
-    return xml_bytes
+    xml_bytes1 = xml_str1.encode(encoding='utf-8')
+    return xml_bytes,xml_bytes1
 
 
 if __name__ == '__main__':
     s = socket_bind()
-    xml_bytes = get_bytes()
+    xml_bytes,xml_bytes1 = get_bytes()
     # 传输的数据的基本信息用bytes_info来记录
     # 消息类型用四位二进制来表示，可以表示16种不同的数据类型
     # 用8位十进制来表示数据长度
+    # bytes_info长度可以扩展，可以加入其他的标记符，比如读写之类的标志符
     # 发送数据前先发送bytes_info
     # bytes不支持索引，可以用bytearray或者str，这里选择str
     str_info = '0000'+str(xml_bytes.__len__()).zfill(8)
+    str_info1 = '0000'+str(xml_bytes1.__len__()).zfill(8)
+    
     bytes_info = str_info.encode('utf-8')
+    bytes_info1 = str_info1.encode('utf-8')
+    
 
     #print(xml_bytes.decode())
     while True:
@@ -48,12 +57,19 @@ if __name__ == '__main__':
         try:
             connection.settimeout(5)
             #此处必须为connection.send,而不是s.send，之前用s.send报错
-            #还是搞不清楚send和sendall的区别，我发送一个1260198bytes的数据也是能够以此发送过去
+            #还是搞不清楚send和sendall的区别，我发送一个1260198bytes的数据也是能够一次send过去
             connection.sendall(bytes_info)
             print('send byte_info')
             connection.sendall(xml_bytes)
             #print("$$$",isinstance(xml_bytes,unicode))
             print('send xml_byte')
+            
+            connection.sendall(bytes_info1)
+            print('send byte_info1')
+            connection.sendall(xml_bytes1)
+            #print("$$$",isinstance(xml_bytes,unicode))
+            print('send xml_byte1') 
+            
         except socket.timeout:
             print('time out')
     
